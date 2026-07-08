db57c7021ba68cd3ba71e1f25298567e56ee8ead
famzyct <famzyct@gmail.com>
2026-07-08 16:34:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
   'https://rexxiez.netlify.app',
-  'https://mofezz.netlify.app',
+  'https://doublew.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
   'https://viralbell.netlify.app',
   'https://jhvnb.vercel.app',
