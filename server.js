84bb2e1506f297161fcac1ffa633319f066f0776
famzyct <famzyct@gmail.com>
2026-07-21 15:02:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'http://127.0.0.1:5000',
-  'https://bklil.netlify.app',
+  'https://wbillbell.netlify.app',
    // add all your trusted frontends here
 ];
 
